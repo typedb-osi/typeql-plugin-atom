1b050495f6ce5fe45d10a0de75eadf804fb7ad6d
Dmitriy Shishkin <shishkindimon@gmail.com>
2018-05-08 02:00:41 +0300

    Initial Commit

---
+++ spec/language-graql-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageGraqlView from '../lib/language-graql-view';
+
+describe('LanguageGraqlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
